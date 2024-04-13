29d13ab10830e56ba1bc4b1fe4c3e6a0931ba0c3
Hal <haleem.muhsin.71@gmail.com>
2024-04-13 15:10:08 +0530

    AuthUI finish

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_3/sign_in.dart';
 
 class AuthUI extends StatefulWidget {
   const AuthUI({Key? key}) : super(key: key);
@@ -10,15 +11,86 @@ class AuthUI extends StatefulWidget {
 class _AuthUIState extends State<AuthUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(10.0), // Adjust the padding as needed
-        child: ClipRRect(
-          borderRadius:
-              BorderRadius.circular(13.0), // Adjust the border radius as needed
-          child: Image.asset(
-            'lib/assets/Auth.png', // Adjust the path to your image asset
-          ),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10.0), // Adjust the padding as needed
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                        13.0), // Adjust the border radius as needed
+                    child: Image.asset(
+                      'lib/assets/Auth.png', // Adjust the path to your image asset
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 220,
+                  left: 0,
+                  right: 0,
+                  child: Padding(
+                    padding:
+                        EdgeInsets.all(10.0), // Adjust the padding as needed
+                    child: ClipRRect(
+                      // Adjust the border radius as needed
+                      child: Image.asset(
+                        'lib/assets/Auth_1.png', // Adjust the path to your image asset
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 230,
+                  left: 0,
+                  right: 0,
+                  child: Padding(
+                    padding:
+                        EdgeInsets.all(10.0), // Adjust the padding as needed
+                    child: ClipRRect(
+                      // Adjust the border radius as needed
+                      child: Image.asset(
+                        'lib/assets/Auth_2.png', // Adjust the path to your image asset
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Text('Start your journey now',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20)),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignIn()),
+                ); // Add your button onPressed logic here
+              },
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all(Size(250, 50)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        18), // Adjust the border radius as needed
+                  ),
+                ),
+              ),
+              child: Text(
+                'Sign In',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+              ),
+            ),
+          ],
         ),
       ),
     );
