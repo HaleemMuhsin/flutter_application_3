d8373d717021e2260a4364c23e27d589ce41793b
Hal <haleem.muhsin.71@gmail.com>
2024-04-13 22:36:16 +0530

    nav bar

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/material.dart';
 
 class HomeUI extends StatefulWidget {
-  const HomeUI({super.key});
+  const HomeUI({Key? key}) : super(key: key);
 
   @override
   State<HomeUI> createState() => _HomeUIState();
 }
 
 class _HomeUIState extends State<HomeUI> {
+  int _selectedIndex = 0; // Add this line to track the selected tab index
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.vertical(
+                top: Radius.circular(20.0),
+                bottom:
+                    Radius.circular(20.0) // Adjust the border radius as needed
+                ),
+            child: BottomNavigationBar(
+              currentIndex: _selectedIndex,
+              onTap: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.edit),
+                  label: 'Edit',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle),
+                  label: 'Profile',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
