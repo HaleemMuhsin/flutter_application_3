dd018d47b724049851fa58cc89a7369ce59d5bb0
Hal <haleem.muhsin.71@gmail.com>
2024-04-16 21:26:06 +0530

    webview

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_3/custom_bottom_nav_bar.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeUI extends StatefulWidget {
   const HomeUI({Key? key}) : super(key: key);
@@ -9,12 +10,21 @@ class HomeUI extends StatefulWidget {
 }
 
 class _HomeUIState extends State<HomeUI> {
-  // Add this line to track the selected tab index
+  late WebViewController
+      _controller; // Add this line to track the webview controller
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        body: WebView(
+          initialUrl:
+              'https://billboardsjcetapp.netlify.app', // Replace with your website URL
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller = webViewController;
+          },
+        ),
         bottomNavigationBar: CustomBottomNavBar(selectedIndex: 1),
       ),
     );
